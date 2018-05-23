6c3eb17e624735fc62ef25a39004899f65a3113e
Jørgen Nordmoen <jorgehn@student.matnat.uio.no>
2018-05-23 11:48:28 +0200

    Updated naming following `dyret_simulation 0.2.0`

--- include/dyret_common/joints.h
+++ include/dyret_common/joints.h
@@ -8,16 +8,16 @@ namespace dyret {
 	// Joint names describe the order which joints are published
 	// in `ServoStateArray.msg` for revolute joints.
 	static const std::string JOINT_NAMES[] = {
-		"dyret::fl_joint1", "dyret::fl_joint2", "dyret::fl_joint3",
-		"dyret::fr_joint1", "dyret::fr_joint2", "dyret::fr_joint3",
-		"dyret::br_joint1", "dyret::br_joint2", "dyret::br_joint3",
-		"dyret::bl_joint1", "dyret::bl_joint2", "dyret::bl_joint3"};
+		"dyret::fl_joint0", "dyret::fl_joint1", "dyret::fl_joint2",
+		"dyret::fr_joint0", "dyret::fr_joint1", "dyret::fr_joint2",
+		"dyret::br_joint0", "dyret::br_joint1", "dyret::br_joint2",
+		"dyret::bl_joint0", "dyret::bl_joint1", "dyret::bl_joint2"};
 
 	// Extension names describe the order which joints are published
 	// in `ServoStateArray.msg` for prismatic joints.
 	static const std::string EXT_NAMES[] = {
-		"dyret::fl_ext1", "dyret::fl_ext2",
-		"dyret::fr_ext1", "dyret::fr_ext2",
-		"dyret::br_ext1", "dyret::br_ext2",
-		"dyret::bl_ext1", "dyret::bl_ext2"};
+		"dyret::fl_ext0", "dyret::fl_ext1",
+		"dyret::fr_ext0", "dyret::fr_ext1",
+		"dyret::br_ext0", "dyret::br_ext1",
+		"dyret::bl_ext0", "dyret::bl_ext1"};
 }
