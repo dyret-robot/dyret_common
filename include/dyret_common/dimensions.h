04c265d717e5f9412020aefdcbdfb7597756d922
tonnesfn <tonnes.nygaard@gmail.com>
2019-07-02 09:53:15 +1000

    Added new updated DyRET body dimensions

--- include/dyret_common/dimensions.h
+++ include/dyret_common/dimensions.h
@@ -7,7 +7,7 @@
   const double L23 = 255.0; // Minimum distance from second to third joint
 
   // Body constants
-  const double frameLength = 475.0; // Distance between servo centers in mm
-  const double frameWidth  = 259.0; // Distance between servo centers in mm
+  const double frameLength = 430.0; // Distance between servo centers in mm
+  const double frameWidth  = 230.0; // Distance between servo centers in mm
 
 #endif
